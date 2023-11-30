a3db4ec8c6f4da75db4eb8539f02f9b44ce27cfa
powerangelito <angelito_dya@hotmail.com>
2023-11-30 01:28:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/member.html',
+    controller: 'MemberController',
+    controllerAs: 'memberCtrl',
+    bindToController: true,
+    scope: {
+      member: '=',
+      skills: '='
+    }
+  };
+}
